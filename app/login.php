0efb8b24ad0a362b7fe69c74df190951193ff047
steven1730 <31947870+steven1730@users.noreply.github.com>
2019-02-12 21:14:09 +0800

    Update login.php

--- app/login.php
+++ app/login.php
@@ -2,5 +2,6 @@
 if (isset($_POST['submitbutton'])){
 	setcookie('username', $_POST['username'], time()+3600);
 	//setcookie('submitbutton', $_POST['submitbutton'], time()+3600);
+	echo 'here'
 	header('location: real_index.php');
 ?>
