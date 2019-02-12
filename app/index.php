96839c2e6baf8af21b635f805a16869fdfc2ac22
steven1730 <31947870+steven1730@users.noreply.github.com>
2019-02-12 17:28:32 +0800

    Update index.php

--- app/index.php
+++ app/index.php
@@ -4,7 +4,7 @@ require('../vendor/autoload.php');
 header('Content-type: image/jpeg');
 
 
-$image_in = new Imagick('test.jpg');
+$image_in = new Imagick('test2.jpg');
 
 $image_in->blurImage(10,10);
 $image_in->borderImage('black', 100, 100);
