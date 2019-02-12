3b8516031a8125c291d5950800d7d6085ef13f39
steven1730 <31947870+steven1730@users.noreply.github.com>
2019-02-12 21:04:04 +0800

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,9 +1,1 @@
 <?php header("Location: login.html"); ?>
-<?php
-if (isset($_COOKIE['username'])){
-	echo 'You are logged as '.$_COOKIE['username'].'<br/>';
-	echo '<a href="logout.php"> Log Out </a>';
-}else{
-	echo 'Hi!';
-}
-?>
