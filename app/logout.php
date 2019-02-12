3ba372f52f41c523be74dd26eca091e82786e16c
steven1730 <31947870+steven1730@users.noreply.github.com>
2019-02-12 23:49:53 +0800

    Update logout.php

--- app/logout.php
+++ app/logout.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_COOKIE['username'])){
 	setcookie('username', '', time()-3600);
-	setcookie('password', '', time()-3600);
+	//setcookie('password', '', time()-3600);
 }
 header('location: login.html');
 ?>
