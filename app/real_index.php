edda729dae876a7a67fe30a4c2f678159c214ee5
steven1730 <31947870+steven1730@users.noreply.github.com>
2019-02-13 21:32:46 +0800

    Update real_index.php

--- app/real_index.php
+++ app/real_index.php
@@ -7,13 +7,4 @@ if (isset($_COOKIE['username'])){
 	echo '<a href="login.html"> Login </a>';
 	echo ' !';
 }
-require('../vendor/autoload.php');
-header('Content-type: image/jpeg');
-
-$image_in = new Imagick('test.jpg');
-
-$image_in->blurImage(10,10);
-$image_in->borderImage('black', 100, 100);
-
-echo $image_in;
 ?>
