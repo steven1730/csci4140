ada2a1e90fce134ec50e9a737c622521011d651a
steven1730 <31947870+steven1730@users.noreply.github.com>
2019-02-13 21:44:42 +0800

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,1 +1,13 @@
-<?php header("location: login.html"); ?>
+<?php 
+	$con = mysql_connect("localhost","mysql.default_user","mysql.default_password");
+	if (!$con){
+		die('Could not connect: '. mysql_error());
+	}
+
+	if (mysql_query("CREATE DATABASE my_db",$con)){
+		echo "Database created";
+	}else{
+		echo "Error creating database: " . mysql_error();
+	}
+	header("location: login.html"); 
+?>
