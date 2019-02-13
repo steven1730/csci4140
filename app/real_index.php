cd7f8cf59e7ffc89b82673c5a7806c081e4e8d1a
steven1730 <31947870+steven1730@users.noreply.github.com>
2019-02-14 00:12:57 +0800

    Update real_index.php

--- app/real_index.php
+++ app/real_index.php
@@ -1,7 +1,6 @@
 <?php
 if (isset($_COOKIE['username'])){
 	echo 'You are logged as '.$_COOKIE['username'].'<br/>';
-	echo $_COOKIE['username'];
 	echo '<a href="logout.php"> Log Out </a>';
 }else{
 	echo 'Hello my guest! Please';
