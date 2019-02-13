edf3f8fc38be252199735c596e6886fa480f868a
steven1730 <31947870+steven1730@users.noreply.github.com>
2019-02-13 22:20:58 +0800

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,7 +1,8 @@
 <?php 
 	$con = mysql_connect("localhost","mysql.default_user","mysql.default_password");
 	if (!$con){
-		die('Could not connect: '. mysql_error());
+		//die('Could not connect: '. mysql_error());
+		echo 'nooooooooooooooooooooooo';
 	}
 
 	//mysql_select_db("DATABASE_URL", $con)
