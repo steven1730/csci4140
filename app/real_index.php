a0c428d2a055754cad9b7882ba5cdd61a3a932f9
steven1730 <31947870+steven1730@users.noreply.github.com>
2019-02-15 14:02:13 +0800

    Update real_index.php

--- app/real_index.php
+++ app/real_index.php
@@ -2,23 +2,37 @@
 if (isset($_COOKIE['username'])){
 	echo 'You are logged as '.$_COOKIE['username'].'<br/>';
 	echo '<a href="logout.php"> Log Out </a>';
+	$db = parse_url(getenv("DATABASE_URL"));
+	echo "hello, db user:";
+	echo $db["user"];
+
+	$pdo = new PDO("pgsql:") .sprintf("host=%s;port=%s;password=%s;dbname=%s", $db["host"], $db["port"], $db["user"], $db["pass"], ltrim(($db["path"], "/")));
+
+	$sql = "INSERT INTO MyUsers (id,name,passwords) VALUES (3,'john','zxc123');";
+
+	if($pdo->query($sql) == TRUE){
+		echo "Table Myguests created successfully";
+	}else{
+		echo "Error creating table: ";
+	}
 }else{
 	echo 'Hello my guest! Please';
 	echo '<a href="login.html"> Login </a>';
 	echo ' !';
+	$db = parse_url(getenv("DATABASE_URL"));
+	echo "hello, db user:";
+	echo $db["user"];
+
+	$pdo = new PDO("pgsql:") .sprintf("host=%s;port=%s;password=%s;dbname=%s", $db["host"], $db["port"], $db["user"], $db["pass"], ltrim(($db["path"], "/")));
+
+	$sql = "INSERT INTO MyUsers (id,name,passwords) VALUES (3,'john','zxc123');";
+
+	if($pdo->query($sql) == TRUE){
+		echo "Table Myguests created successfully";
+	}else{
+		echo "Error creating table: ";
+	}
 }
 
-$db = parse_url(getenv("DATABASE_URL"));
-echo "hello, db user:";
-echo $db["user"];
 
-$pdo = new PDO("pgsql:") .sprintf("host=%s;port=%s;password=%s;dbname=%s", $db["host"], $db["port"], $db["user"], $db["pass"], ltrim(($db["path"], "/")));
-
-$sql = "INSERT INTO MyUsers (id,name,passwords) VALUES (3,'john','zxc123');";
-
-if($pdo->query($sql) == TRUE){
-	echo "Table Myguests created successfully";
-}else{
-	echo "Error creating table: ";
-}
 ?>
