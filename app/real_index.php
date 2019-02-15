3e1f19b3c787cd7ced6c6a26ef2902b3d0065b49
steven1730 <31947870+steven1730@users.noreply.github.com>
2019-02-15 18:52:24 +0800

    Add files via upload

--- app/real_index.php
+++ app/real_index.php
@@ -19,6 +19,9 @@ $pdo = new PDO("pgsql:" . sprintf("host=%s;port=%s;user=%s;password=%s;dbname=%s
 	ltrim($db["path"], "/")
 ));
 
+?>
+
+<?php
 if (isset($_COOKIE['username'])){
 	echo "HERqfefsfw";
 	require('../vendor/autoload.php');
@@ -27,6 +30,7 @@ $s3 = new Aws\S3\S3Client([
     'version'  => '2006-03-01',
     'region'   => 'us-east-2',
 ]);
+echo "wefdefeafsfsfsef";
 $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
 	echo "FSGDS";
 ?>
@@ -43,7 +47,9 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES
         // FIXME: do not use 'name' for upload (that's the original filename from the user's computer)
         $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
 ?>
-        <p>Upload <a href="<?=htmlspecialchars($upload->get('ObjectURL'))?>">successful</a> :)</p>
+        <p>
+        	Upload <a href="<?=htmlspecialchars($upload->get('ObjectURL'))?>">successful</a> :)
+        </p>
 <?php } catch(Exception $e) { ?>
         <p>Upload error :(</p>
 <?php } } ?>
