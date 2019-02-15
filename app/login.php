c18aad536b6b7622f0296dc6fc0cbaa9b9e23e41
steven1730 <31947870+steven1730@users.noreply.github.com>
2019-02-15 14:22:37 +0800

    Add files via upload

--- app/login.php
+++ app/login.php
@@ -1,8 +1,28 @@
 <?php
 if (isset($_POST['button'])){
+	$db = parse_url(getenv("DATABASE_URL"));
+
+	$pdo = new PDO("pgsql:" . sprintf("host=%s;port=%s;user=%s;password=%s;dbname=%s",
+		$db["host"],
+		$db["port"],
+		$db["user"],
+		$db["pass"],
+		ltrim($db["path"], "/")
+	));
+
+	$sql = "SELECT name FROM myusers;";
+	$result = $pdo->query($sql);
+
+	if ($result->num_rows > 0){
+		while ($row = $result->fetch_assoc()){
+			echo "name:" .$row["name"]."<br>";
+		}
+	}
+
 	setcookie('username', $_POST['username'], time()+3600);
 	header('location: real_index.php');
 }else if (isset($_POST['loginasguest'])){
 	header('location: real_index.php');
 }
+
 ?>
--- app/real_index.php
+++ app/real_index.php
@@ -4,8 +4,6 @@ if (isset($_COOKIE['username'])){
 	echo '<a href="logout.php"> Log Out </a>';
 
 	$db = parse_url(getenv("DATABASE_URL"));
-	echo "hello, db user:<br/>";
-	echo $db["user"] . "<br/>";
 
 	$pdo = new PDO("pgsql:" . sprintf("host=%s;port=%s;user=%s;password=%s;dbname=%s",
 		$db["host"],
@@ -15,15 +13,6 @@ if (isset($_COOKIE['username'])){
 		ltrim($db["path"], "/")
 	));
 
-	// Just and example, you can also apply it on upload images or other in fos
-	$sql = "INSERT INTO MyUsers (id,name,passwords) VALUES(3,'John','zxc123');";
-
-	if($pdo->query($sql) == TRUE)
-	{
-		echo "Table MyGuests created successfully <br/>";
-	} else{
-		echo "Error creating table: <br/>";
-	}
 }else{
 	echo 'Hello my guest! Please';
 	echo '<a href="login.html"> Login </a>';
