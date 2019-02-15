f9d467c66b760725975766ee67fef3e7c0b42496
steven1730 <31947870+steven1730@users.noreply.github.com>
2019-02-15 18:35:27 +0800

    Add files via upload

--- app/index.php
+++ app/index.php
@@ -1,3 +1,12 @@
 <?php 
 	header("location: login.html"); 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>sdfghjhgfd</title>
+</head>
+<body>
+
+</body>
+</html>
--- app/real_index.php
+++ app/real_index.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>asdfgh</title>
-</head>
-<body>
-
-</body>
-</html>
-
 <?php
 if (isset($_COOKIE['username'])){
 	echo 'You are logged as '.$_COOKIE['username'].'.';
