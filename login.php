3451f195ea269e4f1a2e617e0e90836e1ff013c7
steven1730 <31947870+steven1730@users.noreply.github.com>
2019-02-15 15:08:48 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,33 @@
+<?php
+$flag = 0
+if (isset($_POST['button'])){
+	$db = parse_url(getenv("DATABASE_URL"));
+
+	$pdo = new PDO("pgsql:" . sprintf("host=%s;port=%s;user=%s;password=%s;dbname=%s",
+		$db["host"],
+		$db["port"],
+		$db["user"],
+		$db["pass"],
+		ltrim($db["path"], "/")
+	));
+	//$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	//$sql = 'SELECT name FROM MyUsers';
+	//$result = $pdo->query($sql);
+	echo "GGGGGGGGG";
+	//$result->setFETCHMode(PDO::FETCH_ASSOC);
+
+	//while ($row = $result->fetch()){
+	//	echo $row['name'];
+	//}
+}
+
+//if ($flag == 1){
+//	setcookie('username', $_POST['username'], time()+3600);
+//	header('location: real_index.php');
+//}
+//else if (isset($_POST['loginasguest'])){
+//	header('location: real_index.php');
+//}
+
+?>
