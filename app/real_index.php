57aa940ed0e797d40c058d5107e2f9bfeb5a361d
steven1730 <31947870+steven1730@users.noreply.github.com>
2019-02-15 14:11:15 +0800

    Update real_index.php

--- app/real_index.php
+++ app/real_index.php
@@ -4,24 +4,25 @@ if (isset($_COOKIE['username'])){
 	echo '<a href="logout.php"> Log Out </a>';
 
 	$db = parse_url(getenv("DATABASE_URL"));
-	echo "hello, db user:";
-	echo $db["user"];
+	echo "hello, db user:<br/>";
+	echo $db["user"] . "<br/>";
 
-	$pdo = new PDO("pgsql:" .sprintf("
-		host=%s;port=%s;user=%s;password=%s;dbname=%s",
-		$db["host"], 
-		$db["port"], 
-		$db["user"], 
-		$db["pass"], 
-		ltrim(($db["path"], "/")
+	$pdo = new PDO("pgsql:" . sprintf("host=%s;port=%s;user=%s;password=%s;dbname=%s",
+		$db["host"],
+		$db["port"],
+		$db["user"],
+		$db["pass"],
+		ltrim($db["path"], "/")
 	));
 
-	$sql = "INSERT INTO MyUsers (id,name,passwords) VALUES (3,'john','zxc123');";
+	// Just and example, you can also apply it on upload images or other in fos
+	$sql = "INSERT INTO MyUsers (id,name,passwords) VALUES(3,'John','zxc123');";
 
-	if($pdo->query($sql) == TRUE){
-		echo "Table Myguests created successfully";
-	}else{
-		echo "Error creating table: ";
+	if($pdo->query($sql) == TRUE)
+	{
+		echo "Table MyGuests created successfully <br/>";
+	} else{
+		echo "Error creating table: <br/>";
 	}
 }else{
 	echo 'Hello my guest! Please';
