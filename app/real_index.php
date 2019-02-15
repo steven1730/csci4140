2a9c1bc5242542f550c4b5e362af6b3fbbc43ed9
steven1730 <31947870+steven1730@users.noreply.github.com>
2019-02-15 16:40:22 +0800

    Add files via upload

--- app/real_index.php
+++ app/real_index.php
@@ -1,23 +1,20 @@
-<?php
-if (isset($_COOKIE['username'])){
-	echo 'You are logged as '.$_COOKIE['username'].'<br/>';
-	echo '<a href="logout.php"> Log Out </a>';
+<!DOCTYPE html>
+<html>
+<body>
+	<h1>Photo Album</h1>
+	<?php
+	if (isset($_COOKIE['username'])){
+		echo 'You are logged as '.$_COOKIE['username'].'.';
+		echo '<a href="logout.php">                               Log Out </a><br/><br/>';
+		echo 'Photo Album<br/>';
+	}else{
+		echo 'Hello my guest! Please';
+		echo '<a href="login.html"> Login </a>';
+		echo ' !';
+	}
 
-	$db = parse_url(getenv("DATABASE_URL"));
-
-	$pdo = new PDO("pgsql:" . sprintf("host=%s;port=%s;user=%s;password=%s;dbname=%s",
-		$db["host"],
-		$db["port"],
-		$db["user"],
-		$db["pass"],
-		ltrim($db["path"], "/")
-	));
-
-}else{
-	echo 'Hello my guest! Please';
-	echo '<a href="login.html"> Login </a>';
-	echo ' !';
-}
+	?>
+</body>
+</html>
 
 
-?>
