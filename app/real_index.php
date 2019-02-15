85c4dd5709765892a525829aa4a54dec16c57688
steven1730 <31947870+steven1730@users.noreply.github.com>
2019-02-15 18:53:31 +0800

    Add files via upload

--- app/real_index.php
+++ app/real_index.php
@@ -26,6 +26,7 @@ if (isset($_COOKIE['username'])){
 	echo "HERqfefsfw";
 	require('../vendor/autoload.php');
 // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
+	echo 'wqdqwafewggergfre';
 $s3 = new Aws\S3\S3Client([
     'version'  => '2006-03-01',
     'region'   => 'us-east-2',
