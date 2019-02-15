7760fc9419539e9cf3b399a36e8bfea0fb16a6e5
steven1730 <31947870+steven1730@users.noreply.github.com>
2019-02-15 15:46:49 +0800

    Add files via upload

--- app/login.php
+++ app/login.php
@@ -12,25 +12,33 @@ if (isset($_POST['button'])){
 	));
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+	$name = $_POST['username'];
 	$sql = 'SELECT name FROM MyUsers';
 	$result = $pdo->query($sql);
 	$result->setFETCHMode(PDO::FETCH_ASSOC);
 	while ($row = $result->fetch()){
-		if ($_POST['username'] == $row['name'])
+		if ($_POST['username'] == $row['name']){
 			$flag = 1;
+			break;
+		}
 	}
-}
-if ($flag == 1){
-	setcookie('username', $_POST['username'], time()+3600);
-	header('location: real_index.php');
-}
-else{
-	//if (isset($_POST['loginasguest'])){
-	//	header('location: login.html');
-	//}
-	echo "No such user! Please";
-	echo '<a href="login.html"> login </a>';
-	echo "again!";
+
+	if ($flag == 1){
+		$sql2 = 'SELECT password FROM MyUsers where name = $name';
+		$result2 = $pdo->query($sql2);
+		$result->setFETCHMode(PDO::FETCH_ASSOC);
+		while ($row = $result->fetch()){
+			if ($_POST['password'] == $row['password']){
+				setcookie('username', $_POST['username'], time()+3600);
+				header('location: real_index.php');
+			}else{
+				echo "No such user! Please";
+				echo '<a href="login.html"> login </a>';
+				echo "again!";
+			}
+		}
+	}
+
 }
 
 ?>
