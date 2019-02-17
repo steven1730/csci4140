4df16e161b4c333676d71daae909c21abeccd09c
steven1730 <31947870+steven1730@users.noreply.github.com>
2019-02-17 22:28:19 +0800

    Add files via upload

--- app/initial.php
+++ app/initial.php
@@ -1,5 +1,17 @@
 <?php
 
+$bucket = 'csci4140-mybucket1';
+
+$keys = $s3->listObjects(['Bucket' => $bucket]) -> getPath('Contents/*/Key');
+
+$s3->deleteObjects([
+    'Bucket'  => $bucket,
+    'Delete' => [
+        'Objects' => array_map(function ($key) {
+            return ['Key' => $key];
+        }, $keys)
+    ],
+]);
 echo 'System was initializated~';
 
 ?>
