1d6b40be5dff1932b1fe119845d30cecb0667952
steven1730 <31947870+steven1730@users.noreply.github.com>
2019-02-17 22:44:02 +0800

    Add files via upload

--- app/initial.php
+++ app/initial.php
@@ -5,15 +5,10 @@ $s3 = new S3Client([['version'=>'2006-03-01','region'=>'ap-northeast-1',]);
 $bucket = 'csci4140-mybucket1';
 
 $result111 = $s3->listObjects(array('Bucket' => $bucket));
+foreach ($result111['Contents'] as $object){
+	$s3->deleteObject(['Bucket' => $bucket,'Key'    => $object['Key']]);
+}
 
-$s3->deleteObjects([
-    'Bucket'  => $bucket,
-    'Delete' => [
-        'Objects' => array_map(function ($key) {
-            return ['Key' => $result111['Contents']['Key']];
-        }, $result111)
-    ],
-]);
 echo 'System was initializated~';
 
 ?>
