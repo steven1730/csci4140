301dd02cf89133722dcce7835fcdaa98bca1e595
steven1730 <31947870+steven1730@users.noreply.github.com>
2019-02-17 22:34:19 +0800

    Add files via upload

--- app/initial.php
+++ app/initial.php
@@ -1,16 +1,13 @@
 <?php
-
+require '..vendor/autoload.php';
+use Aws\S3\S3Client;
+$s3 = new S3Client([['version'=>'2006-03-01','region'=>'ap-northeast-1',]);
 $bucket = 'csci4140-mybucket1';
 
-$keys = $s3->listObjects(['Bucket' => $bucket]) -> getPath('Contents/*/Key');
+$result111 = $s3->listObjects(array('Bucket' => $bucket));
 
 $s3->deleteObjects([
-    'Bucket'  => $bucket,
-    'Delete' => [
-        'Objects' => array_map(function ($key) {
-            return ['Key' => $key];
-        }, $keys)
-    ],
+    'Bucket'  => $bucket
 ]);
 echo 'System was initializated~';
 
