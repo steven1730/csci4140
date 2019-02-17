b29277b80f7db7322770bdac8e37a3e01d582cf5
steven1730 <31947870+steven1730@users.noreply.github.com>
2019-02-17 23:00:18 +0800

    Add files via upload

--- app/initial.php
+++ app/initial.php
@@ -1,11 +1,11 @@
 <?php
-require '..vendor/autoload.php';
-//$s3 = Aws\S3\S3Client(['version'=>'2006-03-01','region'=>'ap-northeast-1',]);
-//$bucketName = 'csci4140-mybucket1';
-//$result111 = $s3->listObjects(array('Bucket' => $bucketName));
-//foreach ($result111['Contents'] as $object){
-//	$s3->deleteObject(['Bucket' => $bucketName,'Key' => $object['Key']]);
-//}
+require ('..vendor/autoload.php');
+$s3 = Aws\S3\S3Client(['version'=>'2006-03-01','region'=>'ap-northeast-1',]);
+$bucketName = 'csci4140-mybucket1';
+$result111 = $s3->listObjects(array('Bucket' => $bucketName));
+foreach ($result111['Contents'] as $object){
+	$s3->deleteObject(['Bucket' => $bucketName,'Key' => $object['Key']]);
+}
 
 echo 'System was initializated~';
 echo '<a href="real_index.php"> Go back to album </a>';
