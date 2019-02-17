131c5ec4c16ec0b5e50647b7e7278011f5f4b905
steven1730 <31947870+steven1730@users.noreply.github.com>
2019-02-17 22:57:04 +0800

    Add files via upload

--- app/initial.php
+++ app/initial.php
@@ -2,7 +2,7 @@
 require '..vendor/autoload.php';
 $s3 = Aws\S3\S3Client(['version'=>'2006-03-01','region'=>'ap-northeast-1',]);
 $bucketName = 'csci4140-mybucket1';
-$result111 = $s3->listObjects(array('Bucket' => $bucketName));
+//$result111 = $s3->listObjects(array('Bucket' => $bucketName));
 //foreach ($result111['Contents'] as $object){
 //	$s3->deleteObject(['Bucket' => $bucketName,'Key' => $object['Key']]);
 //}
